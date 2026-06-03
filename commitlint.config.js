471697996ca22742c2dbf26e67141415a8cc90ea
Daniel Farrell <dfarrell@redhat.com>
2026-06-03 17:59:02 -0400

    Enforce body line length and expand pre-commit checks
    Enable body-max-line-length at 100 chars (was disabled globally
    for Dependabot). Skip commit lint entirely for Dependabot PRs
    instead of weakening rules for everyone.
    
    Expand pre-commit hook with yamllint, shellcheck, and JSON
    validation on staged files. All checks skip gracefully if the
    tool is not installed.
    
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,7 +3,7 @@ module.exports = {
     'header-max-length': [2, 'always', 72],
     'header-min-length': [2, 'always', 10],
     'body-leading-blank': [2, 'always'],
-    'body-max-line-length': [0],
+    'body-max-line-length': [2, 'always', 100],
     'footer-leading-blank': [2, 'always'],
   },
 };
