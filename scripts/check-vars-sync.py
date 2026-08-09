e0b5c17c391387fa61810124fe379fa195a7f1d1
Daniel Farrell <dfarrell@redhat.com>
2026-08-09 05:36:09 -0400

    Multi: audit corrections to packages, system, ssh, and sync script
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- scripts/check-vars-sync.py
+++ scripts/check-vars-sync.py
@@ -112,6 +112,10 @@ CROSS_ROLE_MIRROR_KEYS = {
         REPO_ROOT / "roles/ssh/defaults/main.yml",
         REPO_ROOT / "roles/dotfiles/defaults/main.yml",
     ],
+    "dotfiles_ssh_hardware_key_basename": [
+        REPO_ROOT / "roles/dotfiles/defaults/main.yml",
+        REPO_ROOT / "roles/ssh/defaults/main.yml",
+    ],
     "ssh_ciphers": [
         REPO_ROOT / "roles/system/defaults/main.yml",
         REPO_ROOT / "roles/dotfiles/defaults/main.yml",
